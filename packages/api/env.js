069904544d015856a4c0dae0c6825e4da9fdb371
陈随易 <chenbimo@gmail.com>
2024-12-03 20:07:19 +0800

    修改配置

--- packages/api/env.js
+++ packages/api/env.js
@@ -3,7 +3,7 @@ export const envConfig = {
         NODE_ENV: 'development',
         // mysql 配置
         MYSQL_HOST: '127.0.0.1',
-        MYSQL_DB: 'yicode_bak',
+        MYSQL_DB: 'test',
         MYSQL_USERNAME: 'root',
         MYSQL_PASSWORD: 'root',
         // redis 配置
@@ -11,20 +11,20 @@ export const envConfig = {
         // 上传配置
         LOCAL_DIR: './public/static',
         // 微信配置
-        PAY_NOTIFY_URL: 'https://yicode.mynatapp.cc/api/weixin/payNotify'
+        PAY_NOTIFY_URL: ''
     },
     production: {
         NODE_ENV: 'production',
         // mysql 配置
-        MYSQL_HOST: '10.0.4.6',
-        MYSQL_DB: 'yicode_tech',
-        MYSQL_USERNAME: 'yicode_tech',
-        MYSQL_PASSWORD: 'yicode_tech123456!@#',
+        MYSQL_HOST: '127.0.0.1',
+        MYSQL_DB: 'test',
+        MYSQL_USERNAME: 'root',
+        MYSQL_PASSWORD: 'root',
         // redis 配置
-        REDIS_PASSWORD: 'ca9e1e0f31ca5712854d57ce48d41383',
+        REDIS_PASSWORD: '',
         // 上传配置
-        LOCAL_DIR: '/wwwroot/yicode.tech/static2',
+        LOCAL_DIR: './public/static',
         // 微信配置
-        PAY_NOTIFY_URL: 'https://api.yicode.tech/api/weixin/payNotify'
+        PAY_NOTIFY_URL: ''
     }
 }[process.env.NODE_ENV];
