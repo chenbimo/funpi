5f7d5d22074bac6164459a10bb26041ce1e2496e
陈随易 <chenbimo@gmail.com>
2025-01-19 20:41:53 +0800

    修复导出ajv的问题

--- packages/funpi/funpi.js
+++ packages/funpi/funpi.js
@@ -5,7 +5,7 @@ import Fastify from 'fastify';
 import autoLoad from '@fastify/autoload';
 import fp from 'fastify-plugin';
 import fastifyStatic from '@fastify/static';
-import ajv from 'ajv';
+import Ajv from 'ajv';
 // 启动插件
 import swaggerPlugin from './plugins/swagger.js';
 import loggerPlugin from './plugins/logger.js';
@@ -193,7 +193,7 @@ export {
     fastify,
     fp,
     initServer,
-    ajv,
+    Ajv,
     ajvZh,
     // 环境目录
     appDir,
