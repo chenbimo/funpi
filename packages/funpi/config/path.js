66baba15a49d81f3b09521eea7f87a24f343c238
陈随易 <chenbimo@gmail.com>
2025-03-19 20:05:20 +0800

    删除注释

--- packages/funpi/config/path.js
+++ packages/funpi/config/path.js
@@ -1,4 +1,3 @@
 import { dirname, resolve } from 'path';
 export const appDir = process.cwd();
 export const funpiDir = dirname(dirname(import.meta.filename));
-console.log('🔥[ funpiDir ]-4', funpiDir);
