922b633d4d20f25c5c6afff4b7b1d693533db0c1
陈随易 <chenbimo@gmail.com>
2025-05-05 18:55:12 +0800

    修改接口名称

--- packages/funpi/apis/dict/_meta.js
+++ packages/funpi/apis/dict/_meta.js
@@ -3,13 +3,13 @@ export const metaConfig = {
     apiNames: {
         categoryDelete: '删除字典分类',
         categoryInsert: '添加字典分类',
-        categorySelectAll: '查询所有字典分类',
-        categorySelectPage: '分页查询字典分类',
+        categorySelectAll: '查询字典分类-所有',
+        categorySelectPage: '查询字典分类-分页',
         categoryUpdate: '更新字典分类',
         dictDelete: '删除字典',
         dictInsert: '添加字典',
-        dictSelectAll: '查询所有字典',
-        dictSelectPage: '分页查询所有字典',
+        dictSelectAll: '查询字典-所有',
+        dictSelectPage: '查询字典-分页',
         dictUpdate: '更新字典'
     }
 };
